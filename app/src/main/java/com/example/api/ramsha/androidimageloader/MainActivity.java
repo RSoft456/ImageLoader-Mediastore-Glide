d7bb8ddbecaabc85d586a2f812b2fd49070b0a84
RSoft456 <ramshamehmood456@gmail.com>
2023-01-13 02:05:02 +0500

    imageLoader using glide and mediastore

--- app/src/main/java/com/example/api/ramsha/androidimageloader/MainActivity.java
+++ app/src/main/java/com/example/api/ramsha/androidimageloader/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.api.ramsha.androidimageloader;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button button;
+    Uri uri;
+    private static final int PICK_IMAGE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView =findViewById(R.id.imageView);
+        button =findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, PICK_IMAGE);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            if(data!=null)
+                uri = data.getData();
+            Glide.with(this).load(uri).into(imageView);
+
+        }
     }
 }
